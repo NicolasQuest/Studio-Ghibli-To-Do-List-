d244fde158ada332cab22b4de4e3c0b47a5e106e
Nicolás Pasik <115046877+NicolasQuest@users.noreply.github.com>
2025-04-30 18:53:33 +0200

    Update main.jsx

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import Home from "./components/Home";
 //Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
